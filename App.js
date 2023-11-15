99b43266112d53286454d48be2fc752adb4c1ce3
70Qiang <strongcoderman@gmail.com>
2023-11-15 17:22:12 +0800

    ScrollView 和兼容刘海屏的  SafeAreaView

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
  * @Author: 70Qiang strongcoderman@gmail.com
  * @Date: 2023-11-10 14:40:00
  * @LastEditors: 70Qiang strongcoderman@gmail.com
- * @LastEditTime: 2023-11-15 15:43:07
+ * @LastEditTime: 2023-11-15 15:55:58
  * @FilePath: /AwesomeProject/App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,7 +16,8 @@ import FlexDirection from './SRC_02_Flexbox/FlexDirection'
 // import Index from './SRC_06_ActivityIndicator'
 // import Index from './SRC_07_Image'
 // import Index from './SRC_08_TextInput'
-import Index from './SRC_09_Touchable'
+// import Index from './SRC_09_Touchable'
+import Index from './SRC_10_ScrollView'
 export default class App extends Component {
   render() {
     return (

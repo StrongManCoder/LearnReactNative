abca5661a4c19f88d4d53f18334d51c45159ed77
70Qiang <strongcoderman@gmail.com>
2023-11-21 11:06:42 +0800

    Swiper

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
  * @Author: 70Qiang strongcoderman@gmail.com
  * @Date: 2023-11-10 14:40:00
  * @LastEditors: 70Qiang strongcoderman@gmail.com
- * @LastEditTime: 2023-11-20 15:57:27
+ * @LastEditTime: 2023-11-21 10:44:43
  * @FilePath: /AwesomeProject/App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,7 +31,8 @@ import FlexDirection from './SRC_02_Flexbox/FlexDirection'
 // import Index from './SRC_13_Animated'
 // import Index from './SRC_14_WebView/webViewURL'
 // import Index from './SRC_14_WebView/webViewHtml'
-import Index from './SRC_15_Picker'
+// import Index from './SRC_15_Picker'
+import Index from './SRC_16_Swiper'
 export default class App extends Component {
   render() {
     return (
---
+++ SRC_16_Swiper/index.js
@@ -0,0 +1,59 @@
+/*
+ * @Author: 70Qiang strongcoderman@gmail.com
+ * @Date: 2023-11-21 10:44:12
+ * @LastEditors: 70Qiang strongcoderman@gmail.com
+ * @LastEditTime: 2023-11-21 11:02:53
+ * @FilePath: /AwesomeProject/SRC_16_Swiper/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, Image, Dimensions, ScrollView } from 'react-native'
+import Swiper from 'react-native-swiper'
+
+
+export default class index extends Component {
+  render() {
+    return (
+      <ScrollView>
+
+        {/* //Swiper 必须放在 滚动视图中  */}
+        <Swiper   
+        style={[styles.wrapper]} 
+        showsButtons={true}
+        autoplay={true}
+        >
+       
+        <Image
+        style={[styles.slideImage]}
+        source={require('./images/1.png')}
+        ></Image>
+
+        <Image
+        style={[styles.slideImage]}
+        source={require('./images/2.png')}
+        ></Image>
+
+        <Image
+        style={[styles.slideImage]}
+        source={require('./images/3.png')}
+        ></Image>
+
+        </Swiper>
+
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+    wrapper:{
+        height:200,
+
+    },
+
+    slideImage:{
+        height:200,
+        width:Dimensions.get('window').width,
+    }
+})

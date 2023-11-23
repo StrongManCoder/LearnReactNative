83e2e7c0cbfc5dd4b7daa713686bacb6cb066740
70Qiang <strongcoderman@gmail.com>
2023-11-23 17:28:09 +0800

    页面跳转传参

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
  * @Author: 70Qiang strongcoderman@gmail.com
  * @Date: 2023-11-10 14:40:00
  * @LastEditors: 70Qiang strongcoderman@gmail.com
- * @LastEditTime: 2023-11-23 15:21:17
+ * @LastEditTime: 2023-11-23 16:50:31
  * @FilePath: /AwesomeProject/App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -42,8 +42,9 @@ import FlexDirection from './SRC_02_Flexbox/FlexDirection'
 // import Index from './SRC_21_loading'
 // import Index from './SRC_22_StackNavigator'
 // import Index from './SRC_23_BottomTab'
-import Index from './SRC_25_MaterialTopTabsNavigator'
-import { SafeAreaView } from 'react-native-safe-area-context';
+// import Index from './SRC_25_MaterialTopTabsNavigator'
+import Index from './SRC_27_ PassingParameters'
+
 export default class App extends Component {
   render() {
     return (
---
+++ SRC_27_ PassingParameters/index.js
@@ -0,0 +1,66 @@
+/*
+ * @Author: 70Qiang strongcoderman@gmail.com
+ * @Date: 2023-11-23 16:45:11
+ * @LastEditors: 70Qiang strongcoderman@gmail.com
+ * @LastEditTime: 2023-11-23 16:53:02
+ * @FilePath: /AwesomeProject/SRC_27_ PassingParameters/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { StyleSheet, Text, View ,Button} from 'react-native'
+import React from 'react'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            /* 1. Navigate to the Details route with params */
+            navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
+      </View>
+    );
+  }
+  
+  function DetailsScreen({ route, navigation }) {
+    /* 2. Get the param */
+    const { itemId, otherParam } = route.params;
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() =>
+            navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })
+          }
+        />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+    );
+  }
+
+
+  const Stack = createNativeStackNavigator()
+
+export default function index() {
+  return (
+    <Stack.Navigator>
+    <Stack.Screen name='Home' component={HomeScreen}/>
+    <Stack.Screen name='Details' component={DetailsScreen}/>
+
+    </Stack.Navigator>
+  )
+}
+
+const styles = StyleSheet.create({})
